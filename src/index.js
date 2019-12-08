990ca7b4474bd3511611dcce946e5594629e05ec
Justin Lafferty <lafferjm@gmail.com>
2019-12-07 20:31:29 -0600

    enable auto update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 
+require('update-electron-app')();
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
