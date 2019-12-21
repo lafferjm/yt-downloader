c092f011b50dda03221b6519372d455ceb978485
Justin Lafferty <lafferjm@gmail.com>
2019-12-21 14:42:21 -0600

    added react deps

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+function App() {
+  return <p>This is your app!</p>
+};
+
+export default hot(module)(App);
--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -5,9 +5,20 @@ rules.push({
   use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
 });
 
+rules.push({
+  test: /\.(js|jsx)$/,
+  exclude: /node_modules/,
+  loader: 'babel-loader'
+});
+
 module.exports = {
   // Put your normal webpack config below here
   module: {
     rules,
   },
+  resolve: {
+    alias: {
+      'react-dom': '@hot-loader/react-dom'
+    }
+  }
 };
